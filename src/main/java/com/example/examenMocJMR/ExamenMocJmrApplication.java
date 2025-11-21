4660d2950e6189931b991a41ad4b8b9031e1b854
joelmr26 <luh12152@educastur.es>
2025-11-21 17:24:42 +0100

    Initial commit

---
+++ src/main/java/com/example/examenMocJMR/ExamenMocJmrApplication.java
@@ -0,0 +1,13 @@
+package com.example.examenMocJMR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMocJmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMocJmrApplication.class, args);
+	}
+
+}
